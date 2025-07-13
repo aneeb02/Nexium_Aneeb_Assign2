05e95fe1e4aad9876b4a53b96e42824de406d082
bharajuice <aneeb.02@gmail.com>
2025-07-13 17:09:06 +0500

    fix errors

--- blog-summarizer/src/lib/mongodb.ts
+++ blog-summarizer/src/lib/mongodb.ts
@@ -1,14 +1,21 @@
-import { MongoClient } from "mongodb"
+import { MongoClient } from 'mongodb'
 
-const uri = "mongodb://localhost:27017" // local MongoDB connection
+const uri = process.env.MONGODB_URI!
 const options = {}
 
 let client: MongoClient
-const clientPromise: Promise<MongoClient> =  global._mongoClientPromise
+let clientPromise: Promise<MongoClient>
+
+declare global {
+  // Allow global variable in Node.js context
+  var _mongoClientPromise: Promise<MongoClient> | undefined
+}
 
 if (!global._mongoClientPromise) {
   client = new MongoClient(uri, options)
   global._mongoClientPromise = client.connect()
 }
+
+clientPromise = global._mongoClientPromise
 
 export default clientPromise
